14e5addb76989e71e34a0a7d8962e417f7330f6b
uuNick <nikitasidarenko@gmail.com>
2024-10-19 17:58:09 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Main_section from './components/Main_section/Main_section';
+import Footer from './components/Footer/Footer';
 
 function App() {
   const links = [
     { href: '/home', text: 'Каталог' },
     { href: '/about', text: 'Скидки' },
   ];
+  const additional_pages_links = [
+    { href: 'mailto:nikitasidarenko@gmail.com', text: 'nikitasidarenko@gmail.com'},
+    { href: 'https://uunick.github.io/Deliver-X/tel:+375291234567', text: '902 324 4458'},
+    { href: 'https://github.com/uuNick', text: 'GitHub'},
+  ]
 
   return (
     <div>
@@ -14,6 +20,7 @@ function App() {
       <main>
         <Main_section />
       </main>
+      <Footer main_pages_links={links} additional_pages_links={additional_pages_links}/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Logo from '../../images/logo/logo.svg'
+import data from '../../data/cities.json'
+import './Footer.css';
+
+const Footer = ({ main_pages_links, additional_pages_links }) => {
+    const firstTwo = data.slice(0, 2);
+    const lastTwo = data.slice(-2);
+    return (
+        <footer>
+            <div className="div_deliver_x_dowload">
+                <div className="logo_deliver_x_with_description">
+                    <a href="#" className="logo_in_deliver_x">
+                        <img src={Logo} alt='Logo' />
+                    </a>
+                    <p className="discript_deliver_x">В Delivery X мы гордимся тем, что предоставляем бесперебойную и эффективную логистику предприятиям любого размера.</p>
+                </div>
+                <div className="rectangle_with_buttons_in_deliver_x">
+                    <button className="button_dowload_for_ios">Каталог</button>
+                </div>
+            </div>
+            <div className="horisontal_line">
+            </div>
+            <div className="main_div_footer">
+                <div className="footer_menu">
+                    <h4 className="h4_item_in_footer">Меню</h4>
+                    {main_pages_links.map((link, index) => (
+                        <p key={index} className="item_in_footer"><a className="link_item_in_footer" href={link.href}>{link.text}</a></p>
+                    ))}
+                </div>
+                <div className="utility_pages_footer">
+                    <h4 className="h4_item_in_footer">Полезные страницы</h4>
+                    {additional_pages_links.map((link, index) => (
+                        <p key={index} className="item_in_footer"><a className="link_item_in_footer" target="_blank" href={link.href}>{link.text}</a></p>
+                    ))}
+                </div>
+                <div className="available_in_footer flex align-center">
+                    <h4 className="h4_item_in_footer">Доступно в</h4>
+                    <div className="icons_available_in_1">
+                        {firstTwo.map(item => (
+                            <div className="one_icon_div_in_footer">
+                                <img className="photo_icon_city_in_footer" src={require(`../../images/${item.img}`)} alt={`City ${item.title}`} />
+                                <p className="icon_names_in_footer">{item.title}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="icons_available_in_2">
+                        {lastTwo.map(item => (
+                            <div className="one_icon_div_in_footer">
+                                <img className="photo_icon_city_in_footer" src={require(`../../images/${item.img}`)} alt={`City ${item.title}`} />
+                                <p className="icon_names_in_footer">{item.title}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="horisontal_line">
+            </div>
+            <div className="end_info">
+                <p>
+                    <span className="black_name_in_footer">Авторское право © Deliver X | Разработано</span>
+                    <span className="red_name_in_footer">uuNick</span>
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
